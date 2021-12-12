b145ad3888a81f4a2eacc4c7a5145df002c05cf0
Geoff Johnson <geoff.jay@gmail.com>
2021-12-12 15:12:58 -0800

    feat: add rspec support to rubocop

--- spec/models/incident_spec.rb
+++ spec/models/incident_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 # Test suite for the Incident model
 RSpec.describe Incident, type: :model do
   it 'is valid with valid attributes' do
-    expect(Incident.new).to be_valid
+    expect(described_class.new).to be_valid
   end
 
   # it { should validate_presence_of(:title) }
